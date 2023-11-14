03c38ea814801dbd2877de4ec248dc1ca636e9ed
anushwn <150768181+anushwn@users.noreply.github.com>
2023-11-14 10:48:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var a =document.getElementById("signin1");
+a.addEventListener("click",function(){
+    window.location.href="form.html";
+})
+var b =document.getElementById("cancelbtn");
+b.addEventListener("click",function(){
+    window.location.href="index.html";
+})
---
+++ index2.js
@@ -0,0 +1,7 @@
+var b =document.getElementById("cancelbtn");
+b.addEventListener("click",function(){
+    window.location.href="index.html";
+})
+var a =document.getElementById("login");
+a.addEventListener("click",function(){
+    alert("submitted")})
